44ce8d51a891812e654060da7bc0b279cdd1ecb7
joschroth <joschua.rothenbacher@hfg.design>
2021-04-14 14:34:19 +0200

    added POST endpoint, added readme file

--- index.js
+++ index.js
@@ -1,64 +1,59 @@
-var emoji = require("node-emoji");
+const emoji = require("node-emoji");
 const fs = require("fs").promises;
+const fssync = require("fs");
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
 const length = 3; //define length of our abbreviation
 
-//function to generate random abbreviation based on url input
-function shortenUrl(url) {
+//function to generate random abbreviation with random emojis
+async function shortenUrl(url) {
     //format our string so it doesn't include https:// and .com
-    if (url.includes("/")) {
-        url = url.split("/").pop();
-    }
-    url = url.split(".")[0];
-    var shortened = ""; //create variable for our abbreviation
-    for (var i = 0; i < length; i++) {
+    let shortened = ""; //create variable for our abbreviation
+    for (let i = 0; i < length; i++) {
         shortened = shortened + emoji.random().emoji;
-        // var randomBoolean = Math.random() < 0.5; //generate random boolean to decide wether to use UpperCase or not
-        // if (randomBoolean) {
-        //     //add random letter from root url to our shortened version
-        //     shortened = shortened + url.charAt(Math.floor(Math.random() * url.length)).toUpperCase();
-        // } else {
-        //     shortened = shortened + url.charAt(Math.floor(Math.random() * url.length));
-        // }
     }
+    //save the url + abbrevation in a text file
+    await saveGeneratedAbbrevation(shortened, url);
     return shortened;
 }
-
-//testcode
-var st = [];
-var duplicateCount = 0;
-
-for (var i = 0; i < 1; i++) {
-    var input = "https://google.com";
-    generatedAbbreviation = shortenUrl(input);
-    if (st.includes(generatedAbbreviation)) {
-        console.log("There is a duplicate: " + generatedAbbreviation);
-        duplicateCount++;
-    }
-    st.push(generatedAbbreviation);
-    console.log(generatedAbbreviation);
-    fs.writeFile(
-        "./abbrevationTxt/" + generatedAbbreviation + ".txt",
-        input,
+//save our generated abbrevation to a txt file
+async function saveGeneratedAbbrevation(shortened, url) {
+    await fs.writeFile(
+        "./abbrevationTxt/" + shortened + ".txt",
+        url,
         () => {
-            //Callback - wird aufgerufen wenn die Datei erfolgreich geschrieben wurde
-            console.log("Datei geschrieben!");
+            console.log("File written!");
         }
     );
 }
+app.use(express.json());
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send("service is alive");
+});
+//endpoint to get the original url with the abbrevation
 app.get("/code/:inputcode", async(req, res) => {
     let c = req.params.inputcode;
-    let data = await findURL(c);
-    res.status(200).send("this is the original link: " + data.toString());
+    if (fssync.existsSync("./abbrevationTxt/" + c + ".txt")) {
+        let data = await findURL(c);
+        res.status(200).send("this is the original link: " + data.toString());
+    } else
+        res.status(404).send("couldn't find the link you're looking for");
 });
-
+//endpoint to generate an abbrevation from the url
+app.post('/code/generate', async(req, res) => {
+    console.log(req.body.url);
+    //TODO: check if url is a valid url
+    let short = shortenUrl(req.body.url);
+    res.status(200).send({ url: short }).end();
+});
 app.listen(port, () => {
     console.log(`Joschs app listening at http://localhost:${port}`);
 });
-
+//find  url in txt files
 async function findURL(code) {
     return await fs.readFile("./abbrevationTxt/" + code + ".txt");
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+** Bisher verfügbare Endpoints ** 
+
+- GET /code/:"abkürzung" -Original URL wird zurückgegeben
+- POST /code/generate -Generiert Abkürzung und gibt diese zurück, wenn eine URL mitgegeben wird.
