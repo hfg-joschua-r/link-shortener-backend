db4d17ca9dfbef7f31d3bf0a1251727ebc04f244
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2021-06-01 15:51:02 +0200

    updated backend, code cleanup

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const apiLimiter = rateLimit({
     max: 100, // limit each IP to 100 requests per windowMs
     message: "Maximum amount of API request used" //message send to users if they exceed the number of requests.
 });
+//Function to connect to our database
 async function connectDB() {
     client.connect(err => {
         linksCollection = client.db("linkshortener").collection("links");
@@ -28,17 +29,15 @@ async function connectDB() {
 connectDB();
 
 //function to generate random abbreviation with random emojis
-async function shortenUrl(url) {
+async function shortenUrl() {
     //format our string so it doesn't include https:// and .com
     let shortened = ""; //create variable for our abbreviation
     for (let i = 0; i < length; i++) {
         shortened = shortened + emoji.random().emoji;
     }
-    //save the url + abbrevation in a text file
-    await saveGeneratedAbbrevationDB(shortened, url);
     return shortened;
 }
-//save our generated abbrevation to a txt file
+//save our generated abbrevation in a Database
 async function saveGeneratedAbbrevationDB(shortened, url) {
     linksCollection.insertOne({
         "originalURL": url,
@@ -66,17 +65,23 @@ async function validateURL(uri) {
         return false;
     }
 }
+//Function to update the Counter of each abbrevation and the times it has been viewed
 async function updateLinkClickCounter(linkID, oldClickCounter) {
     oldClickCounter++;
     let newClickVal = { $set: { clickCounter: oldClickCounter } };
     console.log(newClickVal);
+    //Search for the given ID in the Databse
     let o_id = new ObjectId(linkID);
+    //update the entry with the new updated Click Value
     linksCollection.updateOne({ '_id': o_id }, newClickVal, (err, res) => {
         if (err) throw err;
         else {
             console.log("sucessfully updated DB entry");
         }
     });
+}
+async function getDBentryByCode(code) {
+    return await linksCollection.findOne({ shortCode: code });
 }
 app.use(express.json());
 app.use(cors());
@@ -86,33 +91,45 @@ app.get("/", (req, res) => {
 // only apply to requests that begin with /code/
 app.use("/code/generate", apiLimiter);
 
+//test api endpoint
+app.get("/test", async(req, res) => {
+    let data = await getDBentryByCode("abc");
+    console.log(data);
+    res.status(200).send(data).end();
+});
+
+
+
 //endpoint to get the original url with the abbrevation
 app.get("/code/:inputcode", async(req, res) => {
     let code = req.params.inputcode;
-    linksCollection.findOne({ shortCode: code }, (err, result) => {
-        if (err) {
-            res.status(404).send("No DB entry found");
-            throw err;
-        } else {
-            console.log(result.originalURL);
-            updateLinkClickCounter(result._id, result.clickCounter);
-            let updatedClickVal = result.clickCounter + 1;
-            res.status(200).send({ url: result.originalURL, clickCounter: updatedClickVal });
-        }
-    });
+    //find the matching entry in our Databbase
+    let DBentry = await getDBentryByCode(code);
+    if (DBentry === null) {
+        res.status(404).send("No DB entry found");
+    } else {
+        //Update the Link ClickCounter
+        updateLinkClickCounter(DBentry._id, DBentry.clickCounter);
+        //send the original url + updated clickcounter back to frontend
+        let updatedClickVal = DBentry.clickCounter + 1;
+        res.status(200).send({ url: DBentry.originalURL, clickCounter: updatedClickVal });
+    }
 });
 
 //endpoint to generate an abbrevation from the url
 app.post("/code/generate", async(req, res) => {
     let uri = req.body.url;
     console.log(uri);
-    let short = await shortenUrl(uri);
+    let short = await shortenUrl();
+    //check if we have a reachable, valid url
     if (await validateURL(uri)) {
+        //save the URL + ShortenedVersion in Our DB
+        await saveGeneratedAbbrevationDB(short, uri);
         res.status(200).send({ url: short }).end();
     } else {
         res.status(404).send("Given URL is not valid!").end();
     }
 });
 app.listen(port, () => {
-    console.log(`Joschs app listening at http: //localhost:${port}`);
+    console.log(`Joschs app listening at http://localhost:${port}`);
 });
